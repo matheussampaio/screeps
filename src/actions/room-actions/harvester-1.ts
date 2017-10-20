825b40f75afe880af4a5f18d7163a1e590d6085f
Matheus Sampaio <matheus1401@gmail.com>
2017-10-20 08:40:08 -0500

    Try to create better creeps if we a lot of energy and the creep queue is empty

--- src/actions/room-actions/harvester-1.ts
+++ src/actions/room-actions/harvester-1.ts
@@ -2,6 +2,7 @@ import { Agent } from '../../agent'
 import { Priority } from '../../enums'
 import { IAction } from '../../interfaces'
 import { CreateBody } from '../../utils'
+import { Substitute } from '../creep-actions'
 
 export const Harvester1: IAction = {
     name: 'harvester-1',
@@ -28,13 +29,23 @@ export const Harvester1: IAction = {
                     role: 'HarvesterEnergy'
                 },
                 priority: harvesters === 0 ? Priority.VERY_HIGH : Priority.NORMAL,
-                body: new CreateBody({ minimumEnergy: 150, energy: room.energyAvailable })
-                    .addWithMove([WORK], { work: 2 })
-                    .add(CARRY)
-                    .add(WORK, { work: 7 })
-                    .add(MOVE, { move: 7 })
-                    .value()
+                body: createHarvesterBody(room)
             })
+        } else if (hasFreeEnergy(room) && Game.time % 10 === 5) {
+            const body = createHarvesterBody(room)
+            const oldHarvester: Creep | null = canSubstitueForABetterCreep(room, body, 'HarvesterEnergy')
+
+            if (oldHarvester) {
+                room.queueCreep({
+                    body,
+                    memory: {
+                        role: oldHarvester.memory.role,
+                        subTarget: oldHarvester.id,
+                        actions: [[Substitute.name]]
+                    },
+                    priority: harvesters === 0 ? Priority.VERY_HIGH : Priority.NORMAL,
+                })
+            }
         }
 
         let haulers = 0
@@ -50,14 +61,65 @@ export const Harvester1: IAction = {
                     role: 'Hauler'
                 },
                 priority: haulers === 0 ? Priority.VERY_HIGH : Priority.NORMAL,
-                body: new CreateBody({ minimumEnergy: 100, energy: room.energyAvailable })
-                    .addWithMove(CARRY, { carry: 2})
-                    .addWithMove(WORK)
-                    .addWithMove([CARRY, WORK], { carry: 20, work: 5})
-                    .value()
+                body: createHaulerBody(room)
             })
+        } else if (hasFreeEnergy(room) && Game.time % 10 === 2) {
+            const body = createHaulerBody(room)
+            const oldHauler: Creep | null = canSubstitueForABetterCreep(room, body, 'Hauler')
+
+            if (oldHauler) {
+                room.queueCreep({
+                    body,
+                    memory: {
+                        role: oldHauler.memory.role,
+                        subTarget: oldHauler.id,
+                        actions: [[Substitute.name]]
+                    },
+                    priority: harvesters === 0 ? Priority.VERY_HIGH : Priority.NORMAL,
+                })
+            }
         }
 
         return Agent.SHIFT_AND_CONTINUE
     }
+}
+
+function createHarvesterBody(room: Room) {
+    return new CreateBody({ minimumEnergy: 150, energy: room.energyAvailable })
+        .addWithMove([WORK], { work: 2 })
+        .add(CARRY)
+        .add(WORK, { work: 7 })
+        .add(MOVE, { move: 7 })
+        .value()
+}
+
+function createHaulerBody(room: Room) {
+    return new CreateBody({ minimumEnergy: 100, energy: room.energyAvailable })
+        .addWithMove(CARRY, { carry: 2})
+        .addWithMove(WORK)
+        .addWithMove([CARRY, WORK], { carry: 20, work: 5})
+        .value()
+}
+
+function hasFreeEnergy(room: Room) {
+    return room.energyAvailable === room.energyCapacityAvailable && room.memory.queue.length === 0
+}
+
+function canSubstitueForABetterCreep(room: Room, body: string, role: string) {
+    const creeps: Creep[] = _.filter(Game.creeps, (creep: Creep) => {
+        return creep.my && creep.room.name === room.name && creep.memory.role === role
+    })
+    .sort((c1: Creep, c2: Creep) => {
+        if (c1.body.length !== c2.body.length) {
+            return c1.body.length - c2.body.length
+        }
+
+        return c1.ticksToLive - c2.ticksToLive
+    })
+
+    if (creeps.length && body.length > creeps[0].body.length) {
+        return creeps[0]
+    }
+
+    return null
 }
