694ebb8a571d50fdf51c64db2c89ec615af220d2
Matheus Sampaio <matheus1401@gmail.com>
2019-02-01 22:35:33 -0600

    --wip-- [skip ci]

--- packages/ai/rollup.config.js
+++ packages/ai/rollup.config.js
@@ -1,11 +1,33 @@
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
+"use strict";
+
+import clear from "rollup-plugin-clear";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import screeps from "rollup-plugin-screeps";
+
+let cfg;
+const dest = process.env.DEST;
+
+if (!dest) {
+  console.log("No destination specified - code will be compiled but not uploaded");
+} else if ((cfg = require("./screeps.json")[dest]) == null) {
+  throw new Error("Invalid upload destination");
+}
 
 export default {
-  input: './src/index.js',
+  input: "src/index.ts",
   output: {
-    file: './dist/main.js',
-    format: 'cjs'
+    file: "dist/main.js",
+    format: "cjs",
+    sourcemap: true
   },
-  plugins: [resolve(), babel()]
+
+  plugins: [
+    clear({ targets: ["dist"] }),
+    resolve(),
+    commonjs(),
+    typescript({tsconfig: "./tsconfig.json"}),
+    screeps({config: cfg, dryRun: cfg == null})
+  ]
 }
