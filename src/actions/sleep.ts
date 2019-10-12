4f72fb97279d3f31112eddd479195a0890239cd5
Matheus Sampaio <matheus@sampaio.us>
2019-10-12 15:12:07 -0500

    feat(sleep): sleep until a Game tick is reached

--- src/actions/sleep.ts
+++ src/actions/sleep.ts
@@ -2,16 +2,24 @@ import { Action, ACTIONS_RESULT } from '../core'
 
 export interface SleepContext {
   sleepFor: number
+  wakeAt: number
 }
 
 export class Sleep extends Action {
   run(context: SleepContext): [ACTIONS_RESULT, ...string[]] {
-    if (!context.sleepFor) {
-      delete context.sleepFor
-      return [ACTIONS_RESULT.SHIFT_AND_CONTINUE]
+    if (context.wakeAt == null && context.sleepFor == null) {
+      return [ACTIONS_RESULT.SHIFT_AND_STOP]
     }
 
-    context.sleepFor -= 1
+    if (context.wakeAt == null) {
+      delete context.sleepFor
+      context.wakeAt = Game.time + context.sleepFor
+    }
+
+    if (Game.time >= context.wakeAt) {
+      delete context.wakeAt
+      return [ACTIONS_RESULT.SHIFT_AND_STOP]
+    }
 
     return [ACTIONS_RESULT.WAIT_NEXT_TICK]
   }
