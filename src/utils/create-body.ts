da7f78cb146a93ef124264cc893b2752a44f4a93
Matheus Sampaio <matheus@sampaio.us>
2019-10-12 15:06:30 -0500

    refactor CreateBody

--- src/utils/create-body.ts
+++ src/utils/create-body.ts
@@ -1,107 +1,69 @@
 import * as _ from 'lodash'
 
-interface IMaximumParts {
-  [part: string]: number
-}
-
 export class CreateBody {
-  private energy: number
-  private size: number
-  private body: { [p: string] : number }
-  private static readonly MAX_CREEP_SIZE = 50
+  private energyAvailable: number
+  private currentCreepBody: Partial<Record<BodyPartConstant, any>>
 
-  constructor({ minimumEnergy, energy }: { minimumEnergy: number; energy: number }) {
-    this.energy = Math.max(minimumEnergy, energy)
-    this.size = 0
-    this.body = {}
-
-    BODYPARTS_ALL.forEach(part => {
-      this.body[part] = 0
-    })
+  constructor({ minimumEnergy = 150, energyAvailable }: { minimumEnergy: number, energyAvailable: number }) {
+    this.energyAvailable = Math.max(minimumEnergy, energyAvailable)
+    this.currentCreepBody = {}
   }
 
-  public add(parts: BodyPartConstant | BodyPartConstant[], maximum: IMaximumParts = {}): this {
-    if (_.isString(parts)) {
-      parts = [parts]
+  public add(parts: BodyPartConstant[], opts: { withMove?: boolean, repeat?: boolean } = {}): CreateBody {
+    let addedSomeParts = 0
+
+    for (const part of parts) {
+      const partsToAdd = opts.withMove ? [MOVE, part] : [part]
+
+      if (this.canAddPart(partsToAdd)) {
+        this.addParts(partsToAdd)
+
+        addedSomeParts += 1
+      }
     }
 
-    let i = 0
-
-    const minimumCost = _.min(parts.map(part => this.getPartCost(part)))
-
-    while (this.canAddMoreParts(parts, maximum, minimumCost)) {
-      i += 1
-
-      const part = parts[i % parts.length]
-
-      if (this.energy >= this.getPartCost(part) && this.body[part] < (maximum[part] || 1)) {
-        this.body[part] += 1
-        this.size += 1
-        this.energy -= this.getPartCost(part)
-      }
+    if (opts.repeat && addedSomeParts) {
+      return this.add(parts, opts)
     }
 
     return this
   }
 
-  public addWithMove(parts: BodyPartConstant | BodyPartConstant[], maximum: IMaximumParts = {}): this {
-    if (_.isString(parts)) {
-      parts = [parts]
+  private addParts(parts: BodyPartConstant[]): void {
+    for (const part of parts) {
+      this.currentCreepBody[part] = (this.currentCreepBody[part] || 0) + 1
+      this.energyAvailable -= BODYPART_COST[part]
+    }
+  }
+
+  private canAddPart(parts: BodyPartConstant[]): boolean {
+    const partsSumCost: number = _.sum(parts.map(part => BODYPART_COST[part]))
+
+    if (this.energyAvailable < partsSumCost) {
+      return false
     }
 
-    let i = 0
+    const currentCreepSize: number = _.sum(_.values(this.currentCreepBody))
 
-    const minimumCost = _.min(parts.map(part => this.getPartCost(part) + BODYPART_COST[MOVE]))
-
-    while (this.canAddMoreParts(parts, maximum, minimumCost, 1)) {
-      i += 1
-      const part = parts[i % parts.length]
-
-      const energyNeeded = this.getPartCost(part) + BODYPART_COST[MOVE]
-
-      if (this.canAddThisPart(part, energyNeeded, maximum)) {
-        this.body[part] += 1
-        this.body[MOVE] += 1
-        this.size += 2
-        this.energy -= energyNeeded
-      }
+    if (currentCreepSize + parts.length > MAX_CREEP_SIZE) {
+      return false
     }
 
-    return this
+    return true
   }
 
-  private getPartCost(part: BodyPartConstant): number {
-    return BODYPART_COST[part]
-  }
-
-  private canAddThisPart(part: BodyPartConstant, energyNeeded: number, maximum: IMaximumParts): boolean {
-    const hasEnoughEnergy = this.energy >= energyNeeded
-    const limitForThisPartReached = this.body[part] >= (maximum[part] || 1)
-
-    return hasEnoughEnergy && !limitForThisPartReached
-  }
-
-  private canAddMoreParts(
-    parts: BodyPartConstant[],
-    maximum: IMaximumParts,
-    minimumCost: number = 0,
-    movesPart: number = 0
-  ): boolean {
-    return (
-      this.size + movesPart < CreateBody.MAX_CREEP_SIZE &&
-      this.energy >= minimumCost &&
-      parts.some(part => this.body[part] < (maximum[part] || 1))
-    )
-  }
-
-  public value(): BodyPartConstant[] {
+  value(): BodyPartConstant[] {
     const body: BodyPartConstant[] = []
 
-    for (let [part, counter] of Object.entries(this.body)) {
+    for (const part in this.currentCreepBody) {
+      const counter: number = this.currentCreepBody[part as BodyPartConstant]
+
       for (let i = 0; i < counter; i++) {
         body.push(part as BodyPartConstant)
       }
     }
+
+    // @TODO: sort body to destroy unvaluable parts first (i.e. TOUGH)
 
     return body
   }
