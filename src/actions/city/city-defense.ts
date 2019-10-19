580dabedc13ad5f1571c56db8850b93108669e0e
Matheus Sampaio <matheus@sampaio.us>
2019-10-19 15:26:36 -0500

    feat(city-defense): attack creeps with ATTACK parts first

--- src/actions/city/city-defense.ts
+++ src/actions/city/city-defense.ts
@@ -2,40 +2,49 @@ import * as _ from 'lodash'
 
 import { Action, ACTIONS_RESULT } from '../../core'
 import { ICityContext } from './interfaces'
+import * as utils from '../../utils'
 
 export class CityDefense extends Action {
   run(context: ICityContext): [ACTIONS_RESULT, ...string[]] {
     const room: Room = Game.rooms[context.roomName]
 
-    const enemies = room.find(FIND_HOSTILE_CREEPS)
+    const enemies: Creep[] = room.find(FIND_HOSTILE_CREEPS) as Creep[]
 
     if (!enemies.length) {
-      return [ACTIONS_RESULT.WAIT_NEXT_TICK]
+      return this.waitNextTick()
     }
 
     const towers: StructureTower[] = room.find(FIND_MY_STRUCTURES, {
-      filter: t => t.structureType === STRUCTURE_TOWER && t.energy >= 10
+      filter: t => t.structureType === STRUCTURE_TOWER && t.store.getCapacity(RESOURCE_ENERGY) as number >= 10
     }) as StructureTower[]
 
+
     if (towers.length) {
-      const enemy = enemies.sort((e1, e2) => e1.hits - e2.hits)[0]
+      enemies.sort((e1: Creep, e2: Creep) => {
+        const enemy1AttackParts = utils.getActiveBodyPartsFromCreep(e1, ATTACK)
+        const enemy2AttackParts = utils.getActiveBodyPartsFromCreep(e2, ATTACK)
+
+        return enemy2AttackParts - enemy1AttackParts
+      })
+
+      const enemy = enemies[0]
 
       towers.forEach((tower) => {
         tower.attack(enemy)
       })
 
-      return [ACTIONS_RESULT.WAIT_NEXT_TICK]
+      return this.waitNextTick()
     }
 
     if (room.controller && room.controller.safeMode) {
-      return [ACTIONS_RESULT.WAIT_NEXT_TICK]
+      return this.waitNextTick()
     }
 
     if (room.controller && room.controller.safeModeAvailable) {
       room.controller.activateSafeMode()
-      return [ACTIONS_RESULT.WAIT_NEXT_TICK]
+      return this.waitNextTick()
     }
 
-    return [ACTIONS_RESULT.WAIT_NEXT_TICK]
+    return this.waitNextTick()
   }
 }
