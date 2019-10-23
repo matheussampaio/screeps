00943996fa1c577980097f6f22efda1c00d981d5
Matheus Sampaio <matheus@sampaio.us>
2019-10-23 18:34:37 -0500

    refactor: use syntax sugar to return actions results

--- src/actions/creep/creep-check-stop.ts
+++ src/actions/creep/creep-check-stop.ts
@@ -2,32 +2,31 @@ import * as _ from 'lodash'
 
 import { ActionsRegistry, Action, Process, ACTIONS_RESULT } from '../../core'
 import { ICreepContext } from './interfaces'
-import { ISleepContext } from '../sleep'
 
 @ActionsRegistry.register
 export class CreepCheckStop extends Action {
-  run(context: ICreepContext & ISleepContext, process: Process): [ACTIONS_RESULT, ...string[]] {
+  run(context: ICreepContext, process: Process): [ACTIONS_RESULT, ...string[]] {
     const creep: Creep | undefined = Game.creeps[context.creepName]
 
     // stop if we lose access to this creep
     if (creep == null) {
-      return [ACTIONS_RESULT.HALT]
+      return this.halt()
     }
 
     // stop if another process owns this room
     if (creep.memory.PID !== process.PID) {
-      return [ACTIONS_RESULT.HALT]
+      return this.halt()
     }
 
     if (creep.getActiveBodyparts(MOVE) === 0) {
-      return [ACTIONS_RESULT.HALT]
+      return this.halt()
     }
 
     if (creep.spawning) {
-      return [ACTIONS_RESULT.WAIT_NEXT_TICK_ALL]
+      return this.waitNextTickAll()
     }
 
-    return [ACTIONS_RESULT.WAIT_NEXT_TICK]
+    return this.waitNextTick()
   }
 }
 
