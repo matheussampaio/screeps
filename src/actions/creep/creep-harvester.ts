40331f7b1d40ec76d65844c8349d7ba98913812e
Matheus Sampaio <matheus@sampaio.us>
2019-10-24 23:30:55 -0500

    fix(harvester): canceling an order that doesn't exist throws an error

--- src/actions/creep/creep-harvester.ts
+++ src/actions/creep/creep-harvester.ts
@@ -37,7 +37,7 @@ export class CreepHarvester extends Action {
 
     // try to build container every 3rd tick
     if (Game.time % 11 === 0 && container instanceof ConstructionSite && this.canBuildContainer(context, creep)) {
-      creep.cancelOrder('harvest')
+      try { creep.cancelOrder('harvest') } catch {}
       creep.build(container)
     }
 
@@ -48,7 +48,7 @@ export class CreepHarvester extends Action {
 
     // repair container eveery 95 ticks
     if (Game.time % 43 === 0 && container instanceof StructureContainer && container.hits < container.hitsMax - 5000 && this.canBuildContainer(context, creep)) {
-      creep.cancelOrder('harvest')
+      try { creep.cancelOrder('harvest') } catch {}
       creep.repair(container)
     }
 
