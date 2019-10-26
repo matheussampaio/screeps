285b0d1988a98b4d38c2ebc5ac3fe2716005738e
Matheus Sampaio <matheus@sampaio.us>
2019-10-26 15:36:15 -0500

    chore: add .envs and update gulpfile to push code to local private servers

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,23 @@ const screeps = require('gulp-screeps')
 
 dotenv.config()
 
-const credentials = {
-  token: process.env.SCREEPS_TOKEN,
-  branch: process.env.SCREEPS_BRANCH || 'default'
-}
-
 gulp.task('upload', () => {
   return gulp.src('main.js', { cwd: 'dist' })
-    .pipe(screeps(credentials))
+    .pipe(screeps(getOptions()))
 })
+
+function getOptions() {
+  if (process.env.SCREEPS_PRIVATE_SERVER) {
+    return {
+      host: 'localhost',
+      port: 21025,
+      password: process.env.SCREEPS_PASSWORD,
+      email: process.env.SCREEPS_EMAIL
+    }
+  }
+
+  return {
+    token: process.env.SCREEPS_TOKEN,
+    branch: process.env.SCREEPS_BRANCH || 'default'
+  }
+}
