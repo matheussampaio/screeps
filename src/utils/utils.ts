0684c7352d33994c99137c8798e91c2c15346005
Matheus Sampaio <matheus@sampaio.us>
2019-10-26 15:38:56 -0500

    feat(utils): calculate neighbors coordinates

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash'
 
-export function getEmptySpacesAroundPosition(pos: RoomPosition): RoomPosition[] {
-  const positions = getNeighborsPositions(pos)
+export function getEmptySpacesAroundPosition(pos: RoomPosition, range: number = 1): RoomPosition[] {
+  const positions = getNeighborsPositions(pos, range)
 
   const terrain: RoomTerrain = new Room.Terrain(pos.roomName)
 
@@ -10,22 +10,32 @@ export function getEmptySpacesAroundPosition(pos: RoomPosition): RoomPosition[] 
   return positions.filter(p => terrain.get(p.x, p.y) === TERRAIN_MASK_SWAMP || terrain.get(p.x, p.y) === TERRAIN_MASK_PLAIN)
 }
 
-export function getNeighborsPositions(pos: RoomPosition): RoomPosition[] {
-  const cords: [number, number][] = [
-    [-1, -1], [0, -1], [1, -1],
-    [-1,  0],          [1,  0],
-    [-1,  1], [0,  1], [1,  1]
-  ]
+export function getNeighborsPositions(pos: RoomPosition, range: number = 1): RoomPosition[] {
+  const coords = getNeighborsCoords(pos.x, pos.y, range)
 
   const room: Room = Game.rooms[pos.roomName]
 
-  const positions = cords
-    .map(([dx, dy]) => ([pos.x - dx, pos.y - dy]))
-    .map(([x, y]) => room.getPositionAt(x, y))
+  const positions = coords
+    .map(coord => room.getPositionAt(coord.x, coord.y))
     .filter(p => p != null) as RoomPosition[]
 
   return positions
+}
 
+export function getNeighborsCoords(x: number, y: number, range: number = 1): { x: number, y: number}[] {
+  const coords = []
+
+  for (let dx = -range; dx <= range; dx++) {
+    coords.push({ x: x + dx, y: y + range })
+    coords.push({ x: x + dx, y: y - range })
+  }
+
+  for (let dy = -range + 1; dy < range; dy++) {
+    coords.push({ x: x + range, y: y + dy })
+    coords.push({ x: x - range, y: y + dy })
+  }
+
+  return coords.sort((c1, c2) => c1.y !== c2.y ? c1.y - c2.y : c1.x - c2.x)
 }
 
 export function getActiveBodyPartsFromCreep(creeps: Creep | Creep[], part: BodyPartConstant): number {
