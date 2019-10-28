6698041e7ac74ba046b3e39fdca40cc6916b6015
Matheus Sampaio <matheus@sampaio.us>
2019-10-27 19:46:50 -0600

    feat(typed-screeps): use @pyrodogg compilation of WIP PRs

--- bin/download-latest-types.sh
+++ bin/download-latest-types.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O node_modules/@types/screeps/index.d.ts https://raw.githubusercontent.com/EldoranDev/typed-screeps/store-definition-update/dist/index.d.ts
+wget -O node_modules/@types/screeps/index.d.ts https://raw.githubusercontent.com/pyrodogg/typed-screeps/test/dist/index.d.ts
