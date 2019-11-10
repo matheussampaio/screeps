4873032e86070e29ae4c6d5cbafd69c454127d3f
Matheus Sampaio <matheus@sampaio.us>
2019-11-10 12:49:33 -0600

    feat(city-emergency): enable emergency if every creep die

--- src/actions/city/city-emergency.ts
+++ src/actions/city/city-emergency.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash'
 import { ActionsRegistry, PRIORITY } from '../../core'
 import { CreateBody } from '../../utils/create-body'
 import { CreepCheckStop, CreepGeneric } from '../creep'
-import { ICityContext, IPlanSource } from './interfaces'
+import { ICityContext } from './interfaces'
 import { City } from './city'
 import * as utils from '../../utils'
 
@@ -12,45 +12,24 @@ export class CityEmergency extends City {
   run(context: ICityContext) {
     this.context = context
 
-    const creep = Game.creeps[this.emergencyCreep]
-
-    // already in emergency
-    if (creep) {
-      console.log('emergency', creep.ticksToLive)
-      return this.waitNextTick()
+    if (!this.controller.my) {
+      return this.sleep(50)
     }
 
-    let enableEmergencyCreep = true
+    const foundCreepInTheRoom = Object.values(Game.creeps).find(creep => (
+      creep.my && creep.memory.roomName === this.room.name
+    ))
 
-    for (const source of this.sources) {
-      const foundOneHarvesterAlive = source.harvesters.some(creepName => Game.creeps[creepName])
-      const foundOneHaulerAlive = source.haulers.some(creepName => Game.creeps[creepName])
-
-      if (foundOneHarvesterAlive && foundOneHaulerAlive) {
-        enableEmergencyCreep = false
-        break
-      }
-
-      // if (source.linkPos) {
-      //   const pos = this.room.getPositionAt(source.linkPos.x, source.linkPos.y) as RoomPosition
-
-      //   const link = pos.lookFor(LOOK_STRUCTURES).find(s => s.structureType === STRUCTURE_LINK) as StructureLink
-
-      //   if (foundOneHarvesterAlive && link) {
-      //     enableEmergencyCreep = false
-      //     break
-      //   }
-      // }
+    if (foundCreepInTheRoom) {
+      return this.sleep(foundCreepInTheRoom.ticksToLive)
     }
 
-    if (enableEmergencyCreep && this.room.energyAvailable >= 300) {
-      this.createEmergencyCreep(context)
-    }
+    this.createEmergencyCreep()
 
-    return this.sleep(100)
+    return this.sleep(350)
   }
 
-  private createEmergencyCreep(context: ICityContext) {
+  private createEmergencyCreep() {
     const body: BodyPartConstant[] = new CreateBody({ minimumEnergy: 300, energyAvailable: this.room.energyAvailable })
         .add([WORK, CARRY], { repeat: true, withMove: true })
         .value()
@@ -63,8 +42,6 @@ export class CityEmergency extends City {
       priority: PRIORITY.VERY_HIGH,
       memory: {}
     })
-
-    context.emergencyCreep = creepName
   }
 }
 
