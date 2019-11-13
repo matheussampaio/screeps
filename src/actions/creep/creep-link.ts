aee1cb0c331429cda0d6cef9782f6418ac9daf22
Matheus Sampaio <matheus@sampaio.us>
2019-11-12 20:33:48 -0600

    fix(creep-link): calculate which position is near to both link and storage

--- src/actions/creep/creep-link.ts
+++ src/actions/creep/creep-link.ts
@@ -1,46 +1,27 @@
 import * as _ from 'lodash'
 
 import { ActionsRegistry, Action } from '../../core'
+import * as utils from '../../utils'
 
 @ActionsRegistry.register
 export class CreepLink extends Action {
-  private context: any
+  protected context: any
 
   run(context: any) {
     this.context = context
 
-    if (this.storage == null) {
-      return this.sleep(5)
+    if (this.creep == null || this.storage == null || this.link == null) {
+      this.creep.suicide()
+
+      return this.halt()
     }
 
-    if (this.creep == null || this.creep.spawning) {
-      return this.sleep(5)
-    }
-
-    const link = this.getStorageLink()
-
-    if (link == null) {
-      return this.sleep(5)
-    }
-
-    if (!this.creep.pos.isNearTo(link)) {
-      this.creep.travelTo(link)
+    if (this.creep.spawning) {
       return this.waitNextTick()
     }
 
-    if (!this.creep.pos.isNearTo(this.storage)) {
-      this.creep.travelTo(this.storage)
-      return this.waitNextTick()
-    }
-
-    if (!this.creep.store.getFreeCapacity(RESOURCE_ENERGY)) {
-      this.creep.transfer(this.storage, RESOURCE_ENERGY)
-      return this.waitNextTick()
-    }
-
-    if (link.store.getUsedCapacity(RESOURCE_ENERGY)) {
-      this.creep.withdraw(link, RESOURCE_ENERGY)
-      return this.waitNextTick()
+    if (!this.creep.pos.isNearTo(this.link) || !this.creep.pos.isNearTo(this.storage)) {
+      return this.unshiftAndContinue(CreepLinkMoveToPosition.name)
     }
 
     if (this.creep.store.getUsedCapacity(RESOURCE_ENERGY)) {
@@ -48,14 +29,19 @@ export class CreepLink extends Action {
       return this.waitNextTick()
     }
 
+    if (this.link.store.getUsedCapacity(RESOURCE_ENERGY)) {
+      this.creep.withdraw(this.link, RESOURCE_ENERGY)
+      return this.waitNextTick()
+    }
+
     return this.waitNextTick()
   }
 
-  private get creep(): Creep {
+  protected get creep(): Creep {
     return Game.creeps[this.context.creepName]
   }
 
-  private get room(): Room {
+  protected get room(): Room {
     return Game.rooms[this.creep.memory.roomName]
   }
 
@@ -63,7 +49,71 @@ export class CreepLink extends Action {
     return this.room.storage
   }
 
-  private getStorageLink(): StructureLink | null {
+  protected get link(): StructureLink | null {
     return Game.getObjectById(this.context.link)
+  }
+}
+
+@ActionsRegistry.register
+export class CreepLinkMoveToPosition extends CreepLink {
+  run(context: any) {
+    this.context = context
+
+    if (this.creep == null || this.storage == null || this.link == null) {
+      this.creep.suicide()
+
+      return this.halt()
+    }
+
+    if (this.creep.spawning) {
+      return this.waitNextTick()
+    }
+
+    if (this.creep.pos.isNearTo(this.link) && this.creep.pos.isNearTo(this.storage)) {
+      return this.shiftAndContinue()
+    }
+
+    const pos = this.getTargetPosition()
+
+    if (pos == null) {
+      this.logger.error(`Can't find a position closer to Link and Storager`)
+
+      return this.waitNextTick()
+    }
+
+    this.creep.moveTo(pos)
+
+    return this.waitNextTick()
+  }
+
+  private getTargetPosition(): RoomPosition | null {
+    const pos = this.getTargetCoord()
+
+    if (pos == null) {
+      this.logger.error(`Can't find a position closer to Link and Storager`)
+
+      return null
+    }
+
+    return this.room.getPositionAt(pos.x, pos.y)
+  }
+
+  private getTargetCoord(): { x: number, y: number } | null {
+    if (this.link == null || this.storage == null) {
+      return null
+    }
+
+    const neighborsToLink = utils.getNeighborsCoords(this.link.pos.x, this.link.pos.y)
+    const neighborsToStorage = utils.getNeighborsCoords(this.storage.pos.x, this.storage.pos.y)
+
+    for (const p1 of neighborsToLink) {
+      for (const p2 of neighborsToStorage) {
+        if (p1.x === p2.x && p1.y === p2.y) {
+          return p1
+        }
+      }
+    }
+
+    return null
   }
 }
