6411e24981a944a3ad8e6892309fed108bd4fbe2
Matheus Sampaio <matheus@sampaio.us>
2019-11-29 23:41:43 -0600

    --wip-- [skip ci]

--- src/actions/city/city-planner.ts
+++ src/actions/city/city-planner.ts
@@ -6,6 +6,11 @@ import { RectCoordinates, MinCut, Coordinates } from '../../utils/min-cut'
 import { ICityContext } from './interfaces'
 import { City } from './city'
 
+const _visuals: any = {
+  time: 0,
+  visual: {}
+}
+
 @ActionsRegistry.register
 export class CityPlanner extends City {
   run(context: ICityContext) {
@@ -15,6 +20,8 @@ export class CityPlanner extends City {
 
     if (this.planner.map == null) {
       this.replan()
+
+      return this.unshiftAndContinue(CityPlannerStructure.name)
     }
 
     const maxWorkPartAllowedByEnergyCapacity = this.getMaxWorkPartAllowedByEnergyCapacity()
@@ -268,9 +275,7 @@ export class CityPlanner extends City {
     const flag = Game.flags['reset-map']
 
     if (flag && flag.room && flag.room.name === this.context.roomName) {
-      if (this.room.name !== 'sim') {
-        flag.remove()
-      }
+      flag.remove()
 
       delete this.context.planner.map
     }
@@ -389,8 +394,6 @@ export class CityPlanner extends City {
       }
     }
 
-    console.log(`can't find a place for ${structureType}`)
-
     return null
   }
 
@@ -419,3 +422,241 @@ export class CityPlanner extends City {
   }
 }
 
+@ActionsRegistry.register
+export class CityPlannerStructure extends City {
+  run(context: any) {
+    this.context = context
+
+    const costMatrix = new PathFinder.CostMatrix()
+
+    // block tiles around controller
+    utils.getEmptySpacesAroundPosition(this.controller.pos).forEach(pos => {
+      costMatrix.set(pos.x, pos.y, Infinity)
+    })
+
+    const sources = this.room.find(FIND_SOURCES)
+
+    // block tiles around sources
+    for (const source of sources) {
+      utils.getNeighborsPositions(source.pos).forEach(pos => {
+        costMatrix.set(pos.x, pos.y, Infinity)
+      })
+
+      utils.getEmptySpacesAroundPosition(source.pos).forEach(pos => {
+        costMatrix.set(pos.x, pos.y, 5)
+      })
+    }
+
+    const minerals = this.room.find(FIND_MINERALS)
+
+    // block tiles around minerals and place extractor
+    for (const mineral of minerals) {
+      utils.getNeighborsPositions(mineral.pos).forEach(pos => {
+        costMatrix.set(pos.x, pos.y, Infinity)
+      })
+
+      utils.getEmptySpacesAroundPosition(mineral.pos).forEach(pos => {
+        costMatrix.set(pos.x, pos.y, 5)
+      })
+    }
+
+    this.context.planner.targets = [
+      { range: 2, x: this.controller.pos.x, y: this.controller.pos.y, roomName: this.room.name },
+      ...sources.map(source => ({ range: 2, x: source.pos.x, y: source.pos.y, roomName: source.pos.roomName })),
+      ...minerals.map(mineral => ({ range: 2, x: mineral.pos.x, y: mineral.pos.y, roomName: mineral.pos.roomName })),
+      this.room.find(FIND_EXIT_TOP),
+      this.room.find(FIND_EXIT_RIGHT),
+      this.room.find(FIND_EXIT_BOTTOM),
+      this.room.find(FIND_EXIT_LEFT)
+    ]
+
+    const pos = []
+    const terrain = this.room.getTerrain()
+
+    for (let x = 1; x < 49; x++) {
+      for (let y = 1; y < 49; y++) {
+        if (!(terrain.get(x, y) & TERRAIN_MASK_WALL)) {
+          pos.push({ x, y, roomName: this.room.name })
+        }
+      }
+    }
+
+    this.context.planner.targets.push(..._.shuffle(pos))
+
+    this.context.planner.costMatrixSerialized = costMatrix.serialize()
+
+    return this.shiftUnshitAndStop(CityPlannerStructureConsume.name)
+  }
+}
+
+@ActionsRegistry.register
+export class CityPlannerStructureConsume extends City {
+  protected structure?: CostMatrix
+
+  run(context: any) {
+    this.context = context
+
+    if (_visuals.time != Game.time) {
+      _visuals.time = Game.time
+      _visuals.visual = {}
+    }
+
+    this.structure = PathFinder.CostMatrix.deserialize(this.context.planner.costMatrixSerialized)
+
+    const target = this.getNextTarget()
+
+    this.visualizeCostMatrix(this.structure, this.room)
+
+    if (target == null) {
+      if (this.clean(this.structure)) {
+        this.context.planner.costMatrixSerialized = this.structure.serialize()
+
+        if (Game.cpu.getUsed() >= 10) {
+          return this.waitNextTick()
+        } else {
+          return this.retry()
+        }
+      }
+
+      return this.waitNextTick()
+      // return this.shiftAndContinue()
+    }
+
+    const positions = target.map(({ x, y, roomName }: any) => new RoomPosition(x, y, roomName))
+    const range = _.get(target, '[0].range', 0)
+
+    const result = this.search(this.center, positions, this.structure, { range })
+
+    if (!result.incomplete) {
+      result.path.unshift(this.center)
+      this.markPath(this.structure, result.path, this.room)
+      this.visualizePath(result.path, { color: 'green', opacity: 1 })
+    } else {
+    }
+
+    // this.visualizeCostMatrix(this.structure, this.room)
+
+    this.context.planner.costMatrixSerialized = this.structure.serialize()
+
+    if (Game.cpu.getUsed() >= 10) {
+      return this.waitNextTick()
+    } else {
+      return this.retry()
+    }
+  }
+
+  private getNextTarget(): { x: number, y: number }[] | null {
+    if (!this.context.planner.targets.length) {
+      return null
+    }
+
+    const target = this.context.planner.targets.shift()
+
+    if (_.isArray(target)) {
+      return target
+    }
+
+    if (this.structure && !this.structure.get(target.x, target.y)) {
+      return [target]
+    }
+
+    return this.getNextTarget()
+  }
+
+  private search(from: RoomPosition, to: RoomPosition | RoomPosition[], costMatrix: CostMatrix, { range = 0 }: { range?: number } = {}): PathFinderPath {
+    const opts: PathFinderOpts = {
+      roomCallback: (roomName: string): boolean | CostMatrix => {
+        if (roomName === from.roomName) {
+          return costMatrix
+        }
+
+        return false
+      },
+      plainCost: 2,
+      swampCost: 5
+    }
+
+    const positions = _.isArray(to) ? to : [to]
+
+    const goals = positions.map(pos => ({ pos, range }))
+
+    return PathFinder.search(from, goals, opts)
+  }
+
+  private visualizePath(pos: RoomPosition[], style?: LineStyle) {
+    for (let i = 1; i < pos.length; i++) {
+      const from = pos[i - 1]
+      const to = pos[i]
+
+      this.room.visual.line(from.x, from.y, to.x, to.y, style)
+    }
+  }
+
+  private visualizeCostMatrix(costMatrix: CostMatrix, room: Room) {
+    room.visual.circle(this.center.x, this.center.y, { fill: 'gree', radius: 0.5, opacity: 1 })
+
+    for (let x = 0; x < 50; x++) {
+      for (let y = 0; y < 50; y++) {
+        const value = costMatrix.get(x, y)
+
+        if (value >= 25) {
+          if (_visuals.visual[`c-${x},${y}`] == null) {
+            room.visual.circle(x, y, { fill: 'red', radius: 0.5, opacity: 0.3 })
+            _visuals.visual[`c-${x},${y}`] = true
+          }
+        } else if (value === 1) {
+          // console.log('found road')
+          const neighbors = utils.getNeighborsCoords(x, y)
+
+          neighbors.forEach(coord => {
+            const v2 = costMatrix.get(coord.x, coord.y)
+
+            if (v2 === 1) {
+              if (_visuals.visual[`l-${x},${y}-${coord.x},${coord.y}`] == null) {
+                _visuals.visual[`l-${x},${y}-${coord.x},${coord.y}`] = true
+
+                room.visual.line(x, y, coord.x, coord.y, { color: '#111111', opacity: 0.6 })
+              }
+            }
+          })
+        }
+      }
+    }
+  }
+
+  private markPath(costMatrix: CostMatrix, positions: RoomPosition[], room: Room) {
+    const terrain = room.getTerrain()
+
+    for (const pos of positions) {
+      costMatrix.set(pos.x, pos.y, 1)
+
+      utils.getNeighborsPositions(pos)
+        .filter(pos => !(terrain.get(pos.x, pos.y) & TERRAIN_MASK_WALL))
+        .filter(pos => costMatrix.get(pos.x, pos.y) === 0)
+        .forEach(pos => costMatrix.set(pos.x, pos.y, 25))
+    }
+  }
+
+  private clean(costMatrix: CostMatrix): boolean {
+    const arrowCoordsDx = [
+      [[0, 0], [0, -1], [0, 1], [1, 0]],
+      [[0, 0], [1, 0], [-1, 0], [0, 1]],
+      [[0, 0], [0, 1], [0, -1], [-1, 0]],
+      [[0, 0], [-1, 0], [1, 0], [0, -1]],
+    ]
+
+    for (let x = 1; x < 49; x++) {
+      for (let y = 1; y < 49; y++) {
+        const arrow = arrowCoordsDx.some(coords => coords.every(([dx, dy]) => costMatrix.get(x + dx, y + dy) === 1))
+
+        if (arrow) {
+          costMatrix.set(x, y, 25)
+          this.room.visual.circle(x, y, { fill: 'green', opacity: 1, radius: 0.5 })
+          return true
+        }
+      }
+    }
+
+    return false
+  }
+}
