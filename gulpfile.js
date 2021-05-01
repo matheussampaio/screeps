7ea653fa63fccd7185300b018e435af5c141de9d
Matheus Sampaio <matheus@sampaio.us>
2021-04-30 23:57:55 -0500

    chore: update .env to proper handle private server variables

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,20 @@ const screeps = require('gulp-screeps')
 dotenv.config()
 
 gulp.task('upload', () => {
+  const options = getOptions()
+
   return gulp.src('main.js', { cwd: 'dist' })
-    .pipe(screeps(getOptions()))
+    .pipe(screeps(options))
 })
 
 function getOptions() {
   if (process.env.SCREEPS_PRIVATE_SERVER) {
     return {
-      host: 'localhost',
-      port: 21025,
-      password: process.env.SCREEPS_PASSWORD,
-      email: process.env.SCREEPS_EMAIL,
-      branch: process.env.SCREEPS_BRANCH || 'default'
+      host: process.env.SCREEPS_PRIVATE_HOST || 'localhost',
+      port: process.env.SCREEPS_PRIVATE_PORT || 21025,
+      password: process.env.SCREEPS_PRIVATE_PASSWORD,
+      email: process.env.SCREEPS_PRIVATE_EMAIL,
+      branch: process.env.SCREEPS_PRIVATE_BRANCH || 'default'
     }
   }
 
